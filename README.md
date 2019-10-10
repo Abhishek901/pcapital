fceb81fa2fa14a73d9798e6c1fa3fccf712e89b4
abhishekverma <abhishekverma@virtualemployee.com>
2019-10-10 12:28:16 +0530

    adding code files....

---
+++ README.md
@@ -0,0 +1,8 @@
+# Stepas to run
+
+1. Please go the folder which you wanna run with.
+2. Make sure you have installed npm (node package manager in your system).
+3. Run cmd and hit the commend "npm install".
+4. Make sure there is no error in the console after this command.
+5. For runnig the application hit "npm run".
+
---
+++ react-facebook-login/src/components/Welcome/Welcome.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import FacebookLogin from 'react-facebook-login';
+import {PostData} from '../../services/PostData';
+import {Redirect} from 'react-router-dom';
+import './Welcome.css';
+
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginError: false,
+      redirect: false
+    };
+    this.signup = this
+      .signup
+      .bind(this);
+  }
+
+  signup(res, type) {
+    let postData;
+    if (type === 'facebook' && res.email) {
+      postData = {
+        name: res.name,
+        provider: type,
+        email: res.email,
+        provider_id: res.id,
+        token: res.accessToken,
+        provider_pic: res.picture.data.url
+      };
+    }
+
+
+
+    if (postData) {
+      PostData('signup', postData).then((result) => {
+        let responseJson = result;
+        sessionStorage.setItem("userData", JSON.stringify(responseJson));
+        this.setState({redirect: true});
+      });
+    } else {}
+  }
+
+  render() {
+
+    if (this.state.redirect || sessionStorage.getItem('userData')) {
+      return (<Redirect to={'/home'}/>)
+    }
+
+    const responseFacebook = (response) => {
+      console.log("facebook console");
+      console.log(response);
+      this.signup(response, 'facebook');
+    }
+
+  
+    return (
+
+      <div>
+            <FacebookLogin
+              appId="1000102106739993"
+              autoLoad={false}
+              fields="name,email,picture"
+              callback={responseFacebook}/>
+        
+
+           
+
+      </div>
+    );
+  }
+}
+
+export default Welcome;
